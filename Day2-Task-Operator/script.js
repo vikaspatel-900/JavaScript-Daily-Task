b5939ae0fa2440e9bb5f7b289aed04f5c33495f4
vikaspatel900 <www.vikas8269@gmail.com>
2024-05-30 02:05:37 +0530

    Add Day2 task operator

---
+++ Day2-Task-Operator/script.js
@@ -0,0 +1,158 @@
+
+/*-------------------------------------Addition of two numbers --------------------------------------------------------------------*/
+
+let a= 45;
+let b=34;
+let c=a+b;
+document.write("C = "+ c + "<br>")
+
+
+/*----------------------------------Subtraction of two numbers -----------------------------------------------------------------------*/
+
+let ab= 97;
+let bc=56;
+let cd=ab-bc;
+document.write("CD = "+ cd + "<br>")
+
+
+/*--------------------------------------Multiplication of two numbers--------------------------------------------------------------------*/
+
+let abc=25;
+let xyz=20;
+let def=abc*xyz
+document.write("DEF = "+ def + "<br>")
+
+
+
+/*---------------------------------------------division of two numbers--------------------------------------------------------------*/
+
+let x=90;
+let y=10;
+let z=x/y;
+document.write("Z = "+ z + "<br>")
+
+
+/*--------------------------------------------------modulas ---------------------------------------------------------------------*/
+
+let p=45;
+let q=7;
+let r=p%7
+document.write("R = "+ r + "<br>")
+
+
+
+
+/*------------------------------------------------------Equal to------------------------------------------------------*/
+
+let v=42;
+let w="42";
+let s=42
+
+document.write("V= =W - " + (v==w) + "<br>")                       
+document.write( "V= = =W - " +(v===w) + "<br>")
+document.write("V= = =S - " +(v===s)+ "<br>")
+
+
+
+
+/*-------------------------------------------- not equal to -------------------------------------------------*/
+
+let ravi=45;
+let abhi=43;
+document.write((ravi!=abhi)+"<br>")
+
+
+
+/*************************************************************greater  than------------------------------------------*/
+
+let manoj=78;
+let rahul=52;
+
+document.write((manoj>rahul)+"<br>");
+
+
+/*---------------------------------------------greter than equal to & less than equal to-------------------------------*/
+
+let firstN=65;
+let secN=43;
+document.write("FirstN is greter than = "+(firstN>=secN)+"<br>");
+document.write("secN is greter than = "+(firstN<=secN)+"<br>")
+
+
+
+
+
+/*---------------------------------------------------logical operator -----------------------------------------------------*/
+
+
+let str=43;
+let tvs=34;
+document.write((str>10 && tvs>10)+"<br>" );
+
+
+
+
+
+let stp=56;
+let pqr=43;
+document.write((stp>pqr || stp<pqr)+"<br>")
+
+
+
+
+
+let h=30;
+document.write((h!=30)+"<br>")
+
+
+
+
+
+
+/*------------------------------------------------------------Assignment operator ------------------------------------------------------*/
+
+let g;
+g=5;
+document.write("g  = "+g+"<br>");
+
+
+
+
+
+let M=10;
+M+=5
+document.write("M = "+M +"<br>")
+
+
+
+
+let O=3;
+O*=2
+document.write("O = "+O+"<br>")
+
+
+
+
+
+
+/*------------------------------------------------------Ternery operator --------------------------------------*/
+
+
+let num=34;
+let result= num==-34?"num is non-positive" : "num is positive";
+document.write(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
