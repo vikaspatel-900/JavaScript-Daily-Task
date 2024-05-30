7558d8f2f1d3b2f1d6963ddb11ae091bcf73e9cc
vikaspatel900 <www.vikas8269@gmail.com>
2024-05-30 14:02:10 +0530

    Add day3 task file

---
+++ Day3-Task-Conditionlal-Statement/script.js
@@ -0,0 +1,68 @@
+// let age=prompt("Enter your age.........!");
+// age=Number.parseInt(age);
+
+// if(age>=10 && age<=20){
+//     document.write("person is valid")
+// }
+
+// else{
+//     document.write("person is not valid")
+// }
+
+//-------------------------------------------------------------2nd Questions ------------------------------------------------
+
+// let month=prompt("Plase Enter Current Month");
+// month=Number.parseInt(month);
+
+// switch(month){
+//     case (1):
+//         alert("jaunary");
+//         break;
+
+//         case (2):
+//             alert("ferbary");
+//         break;
+    
+//      case (3):
+//         alert("march");
+//                 break;
+
+//      case (4):
+//         alert("April");
+//         break;
+
+//      case (5):
+//         alert("May");
+//             break
+    
+// }
+
+
+
+
+//------------------------------------------------------3rd Question-----------------------------------------------------------
+
+
+// let num=prompt("Please Enter your Number");
+//   num= Number.parseInt(num);
+
+//   if(num%2==0 && num%3==0){
+//       document.write("Number is divisible by 2 & 3 = " + num)
+//   }
+
+
+//   else{
+//     document.write("Number is not divisible by 2 & 3 = " + num)
+//   }
+
+
+//---------------------------------------------------4th Question--------------------------------------------------------------
+
+
+
+let age=prompt("Please Enter Your Age");
+    age=Number.parseInt(age);
+
+
+    let result= age>=18?"You can Drive " : "You Can Not Drive"
+    document.write(result)
