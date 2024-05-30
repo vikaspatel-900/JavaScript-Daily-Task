07a12f47b1b57be352e2229da5f699d912f9ac47
vikaspatel900 <www.vikas8269@gmail.com>
2024-05-30 23:36:07 +0530

    Add day5 task folder

---
+++ Day5-Task-Loop/script.js
@@ -0,0 +1,219 @@
+// let a=prompt("Enter a number");
+// let i=1;
+// while(i<=10){
+//     let j=a*i;
+//     document.write(j+"<br>")
+//     i++;
+// }
+
+
+
+
+
+
+//------------------------------------------- 3rd Q ---------------------------------------------
+// let num=prompt("please enter a number")
+
+// let multi=1;
+
+// while(num>=1){
+//     multi=multi*num;
+//     num--;
+// }
+
+// document.write(multi);
+
+
+//--------------------------------------------- 4th Q -----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------5th Q --------------------------------------------------------
+
+// let num=prompt("please enter a number");
+
+// if(num%2==0){
+//     document.write( num +" is even number ")
+// }
+// else{
+//     document.write(num + " is odd number")
+// }
+
+
+
+
+
+
+//---------------------------------------------7th Q ----------------------------------------------
+
+// let num=prompt("Enter a number");
+
+// let i=1;
+
+// while(i<=10){
+//     let v=num*i;
+//     i++;
+//     document.write(v+"<br>")
+// }
+
+
+
+
+
+//----------------------------------------------8th Q -------------------------------------------------
+
+
+// let num=prompt("Please enter a number");
+
+// if(num<100){
+//     document.write(num + " is smallest from 100")
+// }
+
+// else if(num==100){
+//     document.write(num+ " is equal to 100")
+// }
+
+// else{
+//     document.write(num+ " is bigger from 100")
+// }
+
+
+
+
+//-------------------------------------------------9th Q -------------------------------------------------
+
+
+// let year=prompt("please enter year");
+
+// if(year%4==0){
+//     document.write(year + " is leep year")
+// }
+
+// else{
+//     document.write(year+ " is not leep year")
+// }
+
+
+
+
+//----------------------------------------------------10th Q ------------------------------------------------
+
+
+
+// let num=prompt("Please enter a number");
+// let i=1;
+// let sum=0
+
+// while(i<=num){
+//      sum=sum+i
+//      i++;
+// }
+
+// document.write(sum)
+
+
+
+
+//--------------------------------------------------------12th-----------------------------------------------
+
+
+// let a=0;
+
+// while(a<=15){
+//     if(a%2==0){
+//         document.write(a+ " is even")
+//     }
+//     else{
+//         document.write(a + " is odd")
+//     }
+//     document.write("<br>")
+//     a++;
+// }
+
+
+
+
+
+//-------------------------------------------------13th---------------------------------------------------------
+
+
+// let a=1;
+// while(a<=100){
+//     if(a%3==0){
+//         document.write(a+ " Fizz")
+//     }
+
+//     else if(a%5==0){
+//         document.write(a + " Buzz")
+//     }
+
+//     else if(a%3==0 && a%5==5){
+//         document.write(a+ " FizzBuzz")
+//     }
+
+//     else{
+//         document.write(a)
+//     }
+//     document.write("<br>")
+//     document.write("<br>")
+//     a++;
+// }
+
+
+
+//---------------------------------------------15th-------------------------------------------
+
+// let i=1;
+// let sum=0
+
+// while(i<=1000){
+//     if(i%3==0 && i%5==0){
+//         sum=sum+i
+//     }
+//     i++;
+// }
+
+// document.write(sum)
+
+
+
+
+//--------------------------------------------------16th-----------------------------------------
+
+
+// let a=7;
+// while(a<=31){
+//     if(a%2==0){
+//         document.write(a+ "<br>")
+//     }
+//     a++;
+// }
+
+
+
+//-------------------------------------------------17th-------------------------------------------------
+
+
+// let a=10;
+
+// while(a<=20){
+//     if(a%2==1){
+//         document.write(a+ " <br>")
+//     }
+
+//     a++
+// }
+
+
