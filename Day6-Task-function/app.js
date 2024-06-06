ca6cce6c60a19f829b2cea0ae7113780bd056148
vikaspatel900 <www.vikas8269@gmail.com>
2024-06-06 14:00:54 +0530

    Add day6

--- Day6-Task-function/app.js
+++ Day6-Task-function/app.js
@@ -132,23 +132,23 @@
 //-------------------------------------------------------------------7th---------------------------------------------------
 
 
-let pattern=(e)=>{
+// let pattern=(e)=>{
 
-    for(let i=1; i<=e; i=i+2){
+//     for(let i=1; i<=e; i=i+2){
 
-        let num=i
-     for(let a=1; a<=i; a++){
-        num=num+1
-        document.write(num)
-     }
+//         let num=i
+//      for(let a=1; a<=i; a++){
+//         num=num+1
+//         document.write(num)
+//      }
 
-     document.write("<br>")
+//      document.write("<br>")
 
-    }
+//     }
 
-}
+// }
 
-pattern(6)
+// pattern(6)
 
 
 
@@ -212,24 +212,186 @@ pattern(6)
 
 
 
- let patter=(e)=>{
+//  let patter=(e)=>{
 
-  for(let i=1; i<=e; i=i+2){
-     for(let a=1; a<=i; a++){
-        document.write("*")
-     }
-     document.write("<br>")
+//   for(let i=1; i<=e; i=i+2){
+//      for(let a=1; a<=i; a++){
+//         document.write("*")
+//      }
+//      document.write("<br>")
 
-  }
+//   }
 
- }
-
-
- patter(15)
+//  }
 
 
+//  patter(15)
 
 
 
+//------------------------------------------------------------------12th--------------------------------------------------------------
 
 
+// let pattern=(e)=>{
+          
+// for(let i=e; i>=1; i--){
+//    for(let a=i; a>=1; a--){
+//       document.write(a)
+//    }
+//    document.write("<br>")
+// }
+
+// }
+
+// pattern(5);
+
+
+
+
+//-------------------------------------------------------------------16th-------------------------------------------------------------
+
+
+// let num1=prompt("Enter Your first number");
+// let num2=prompt("Enter your second number");
+
+// const Addition=(a,b)=>{
+//    let c=a*b
+//    document.write(c)
+// };
+
+// Addition(num1,num2)
+
+
+
+
+//------------------------------------------------------division----------------------------------
+
+
+// let num1=prompt("Enter Your first number");
+// let num2=prompt("Enter your second number");
+
+// const Addition=(a,b)=>{
+//    let c=a/b
+//    document.write(c)
+// };
+
+// Addition(num1,num2)
+
+
+
+//--------------------------------------------------------------subtraction-----------------------
+
+// let num1=prompt("Enter Your first number");
+// let num2=prompt("Enter your second number");
+
+// const Addition=(a,b)=>{
+//    let c=a-b
+//    document.write(c)
+// };
+
+// Addition(num1,num2)
+
+
+
+
+//---------------------------------------------------------addition----------------------------------------------
+
+
+
+
+// let num1=prompt("Enter Your first number");
+// let num2=prompt("Enter your second number");
+
+// const Addition=(a,b)=>{
+//    let c=a+b
+//    document.write(c)
+// };
+
+// Addition(num1,num2)
+
+
+
+//---------------------------------------------------------------19th-----------------------------------------------------------
+
+
+// const operation=(a,b)=>{
+//         if(a<b){
+//          for(let i=a+1; i<b; i++ ){
+//             document.write(i+" , ")
+//          }
+//         }
+
+//         else if(a>b){
+//          for(let i=b+1; i<a; i++){
+//             document.write(i + " , ")
+//          }
+//         }
+// }
+
+
+// operation(10,98)
+
+
+
+//------------------------------------------------------------------20------------------------------------------------------
+
+
+
+// let number=(e)=>{
+//       document.write(e**2)
+// }
+
+
+// number(30)
+
+
+
+
+
+
+//------------------------------------------------------------------28th--------------------------------------------------------
+
+
+// let num1=prompt("Please Enter Your Base Number");
+// let num2=prompt("Please Enter A Exponent number")
+
+// let exponent=(a,b)=>{
+
+//    document.write(a**b)
+
+// }
+
+// exponent(num1,num2)
+
+
+
+
+
+
+//-------------------------------------------------------------------30----------------------------------------------------------
+
+
+// let Unum1=prompt("Enter first number");
+// let Unum2=prompt("enter second number")
+
+
+// let operation=(a,b)=>{
+//    document.write(a*b)
+// }
+
+// operation(Unum1,Unum2)
+
+
+
+
+
+
+// let Unum1=prompt("Enter first number");
+// let Unum2=prompt("enter second number")
+
+
+// let operation=(a,b)=>{
+//    document.write(a/b)
+// }
+
+// operation(Unum1,Unum2)
