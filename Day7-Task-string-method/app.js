26af1851466a6cc3587f086109dd88131256dcd8
vikaspatel900 <www.vikas8269@gmail.com>
2024-06-07 12:27:42 +0530

    Add day7 task file

---
+++ Day7-Task-string-method/app.js
@@ -0,0 +1,116 @@
+//------------------------------------------------------------1st-------------------------------------------------------------------------
+
+
+// let str = "This    is   an    example   string.";
+// let result = str.replace(/\s+/g, ' ');
+// document.write(result)
+
+
+
+
+
+//---------------------------------------------------------------2nd--------------------------------------------------------------------------
+
+
+// let str= "Hello world! Welcome to the world of JavaScript.";
+
+// let result=str.replace(/world/g,"hii")
+
+// document.write(result)
+
+
+
+
+//-------------------------------------------------------------------3rd----------------------------------------------------------------
+
+
+
+
+// let str="Hello world! Welcome to the world of JavaScript."
+// let str2=`Hello world! Welcome to the world of JavaScript.`
+
+// document.write(str)
+// document.write(str2)
+
+
+
+//------------------------------------------------------------4th---------------------------------------------------------------------
+
+
+// let string1 = "Hello";
+// let string2 = "Hello";
+
+// if (string1 === string2) {
+//     document.write("The strings are equal.");
+// } else {
+//     document.write("The strings are not equal.");
+// }
+
+
+
+
+
+//------------------------------------------------------------5th----------------------------------------------------------------
+
+
+
+// let str = "123";
+// let num = Number(str);
+
+// document.write(typeof num)
+
+
+
+
+//--------------------------------------------------------6th----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------------9th--------------------------------------------------------------
+
+
+// let str="Hello world! Welcome to the world of JavaScript."
+// let result=str.slice(0,3)
+// document.write(result)
+
+
+
+
+
+
+//----------------------------------------------------------------------------------11th-----------------------------------------------------
+
+
+// let str="Hello world! Welcome to the world of JavaScript."
+
+// let result=str.length
+// document.write(result+"<br>")
+
+// let halfUp=str.slice(0,24)
+// let halfLo=str.slice(24,48)
+// document.write(halfUp.toUpperCase() + halfLo.toLowerCase())
+
+
+
+//-----------------------------------------------------------------------------------14th--------------------------------------------------------
+
+
+
+// let str="Hello world! Welcome to the world of JavaScript."
+// let newStr = str.replace("world", "");
+
+// document.write(newStr)
+
+
+
+//--------------------------------------------------------------------------16th------------------------------------------------------
+
+
+// let str="Hello world! Welcome to the world of JavaScript."
